d7beb507f8665be38250d20a6f8c66591dbef733
David Banham <david@banham.id.au>
2018-03-21 10:10:58 +1100

    Add caching to go steps

--- makefiles/go.mk
+++ makefiles/go.mk
@@ -1,11 +1,11 @@
 .PHONY: build lint run vet test check
 
-build:
+build: $(wildcard $(shell find . -type f | grep "\.go"))
 	CGO_ENABLED=0 go build -o ./bin/$(name) -a -installsuffix cgo -ldflags '-s' .
 
 # https://github.com/golang/lint
 # go get github.com/golang/lint/golint
-lint:
+lint: $(wildcard $(shell find . -type f | grep "\.go"))
 	golint `go list ./... | grep -v /vendor/`
 
 run: build
@@ -13,7 +13,7 @@ run: build
 
 # http://godoc.org/code.google.com/p/go.tools/cmd/vet
 # go get code.google.com/p/go.tools/cmd/vet
-vet:
+vet: $(wildcard $(shell find . -type f | grep "\.go"))
 	go vet `go list ./... | grep -v /vendor/`
 
 check: test vet lint
