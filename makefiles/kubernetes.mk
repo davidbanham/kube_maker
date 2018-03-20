02afbc11ba4c992094619ff53255c9a3d8866069
David Banham <david@banham.id.au>
2018-03-21 10:06:50 +1100

    Semi-automate secret decryption at deploy time

--- makefiles/kubernetes.mk
+++ makefiles/kubernetes.mk
@@ -1,35 +1,42 @@
-.PHONY: k8s_deploy docker_image_build build_manifest registry_push kube_deploy get_exposed_ip teardown_staging teardown_development demand_clean env_secret
-
 stage = dummy
 pull_policy = Always
 uniq:=$(shell uuidgen)
 tag = $(shell whoami)-dev-$(uniq)
 
-k8s_deploy: create_namespace env_secret docker_image_build registry_push build_manifest kube_deploy get_exposed_ip
+.PHONY: k8s_deploy
+k8s_deploy: secrets create_namespace env_secret docker_image_build registry_push build_manifest kube_deploy get_exposed_ip
 
+.PHONY: docker_image_build
 docker_image_build:
 	docker build --tag $(name) .
 	docker tag $(name):latest gcr.io/$(project)/$(prefix)$(name):$(tag)
 
+.PHONY: build_manifest
 build_manifest:
 	cat ./manifest_template.yaml | sed s/__PULL_POLICY__/$(pull_policy)/g | sed s/__NAMESPACE__/$(prefix)$(stage)/g | sed s/__STAGE__/$(stage)/g | sed s/__IMAGE__/$(prefix)$(name)/g | sed s/__NAME__/$(name)/g | sed s/__PROJECT__/$(project)/g | sed s/__HASH_TAG__/$(tag)/ > ./manifest.yaml
 
+.PHONY: registry_push
 registry_push:
 	gcloud docker -- push gcr.io/$(project)/$(prefix)$(name):$(tag)
 
+.PHONY: kube_deploy
 kube_deploy:
 	kubectl apply -f ./manifest.yaml
 
+.PHONY: get_exposed_ip
 get_exposed_ip:
 	@echo "Deployment available at:"
 	@kubectl get service $(name) -o json --namespace $(prefix)$(stage)| jq ".status.loadBalancer.ingress[].ip" | sed s/\"//g
 
+.PHONY: teardown_staging
 teardown_staging: areyousure
 	kubectl delete namespace $(prefix)staging
 
+.PHONY: teardown_development
 teardown_development: areyousure
 	kubectl delete namespace $(prefix)development
 
+.PHONY: demand_clean
 demand_clean:
 	@# Check there are no forbidden extensions not tracked by git
 	git ls-files --others --exclude-standard | grep -E $(forbidden_untracked_extensions) | xargs -n 1 test -z
@@ -40,8 +47,10 @@ demand_clean:
 	$(eval pull_policy=IfNotPresent)
 	$(eval tag=`git rev-parse HEAD`)
 
+.PHONY: env_secret
 env_secret:
 	-cat $(stage).env | xargs printf -- '--from-literal=%s ' | xargs kubectl create secret generic env --dry-run -o yaml | kubectl apply --namespace $(prefix)$(stage) -f -
 
+.PHONY: create_namespace
 create_namespace:
 	-kubectl create namespace $(prefix)$(stage)
--- makefiles/secrets.mk
+++ makefiles/secrets.mk
@@ -1,15 +1,26 @@
 keybase_users = `keybase team list-members $(keybase_team) | awk '{print $$3}' | xargs`
 
-production.env:
+production.env: production.env.encrypted
+	-cp production.env backup.production.env
 	keybase decrypt < production.env.encrypted > production.env
 
-staging.env:
+staging.env: staging.env.encrypted
+	-cp staging.env backup.staging.env
 	keybase decrypt < staging.env.encrypted > staging.env
 
-development.env:
+development.env: staging.env.encrypted
+	-cp development.env backup.development.env
 	keybase decrypt < development.env.encrypted > development.env
 
-encrypted_secrets: production.env.encrypted staging.env.encrypted development.env.encrypted
+.PHONY: secrets
+secrets: production.env staging.env development.env production.env.encrypted staging.env.encrypted development.env.encrypted
+
+.PHONY: encrypt_env_secrets
+encrypt_env_secrets: remove_encrypted_env_secrets production.env.encrypted staging.env.encrypted development.env.encrypted
+
+.PHONY: remove_encrypted_env_secrets
+remove_encrypted_env_secrets: areyousure
+	rm production.env.encrypted staging.env.encrypted development.env.encrypted
 
 production.env.encrypted:
 	keybase encrypt $(keybase_users) < production.env > production.env.encrypted
